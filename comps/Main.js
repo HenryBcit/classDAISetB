4a20525f9449c512a6628ad2dacac9d3cf7ef770
bin <32521853+quadclipse@users.noreply.github.com>
2019-10-02 11:34:52 -0700

    Revert "Bin's FInal Request"

--- comps/Main.js
+++ comps/Main.js
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import Page1 from './Page1';
-import Bin from '../bin/Bin';
+upasana
 import Upasana from '../Upasana/Upasana';  
 import Nico from '../Nico/Nico';
+
 import Matt from '../matt/matt';
+
 import Micah from '../Micah/Micah';
+master
 import AComp from '../andrew/andrewComp';
+
 
 function Main(){
   const [showMenu, setShowMenu] = useState(false);
@@ -60,7 +64,6 @@ function Main(){
       />
       {menu}
       {page1}
-      <Bin />
     </View>
   )
 }
