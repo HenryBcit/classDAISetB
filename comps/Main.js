370925c9da30a2d0f43bccc2cdd7e61e327b0212
NicolasAsis <nico.asis15@gmail.com>
2019-10-02 11:13:30 -0700

    Adding Comp

--- comps/Main.js
+++ comps/Main.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import Page1 from './Page1';
+import Nico from '../Nico/Nico';
 
 function Main(){
   const [showMenu, setShowMenu] = useState(false);
